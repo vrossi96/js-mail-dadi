77b86c2d2c014cd93cef5601464e154c6f35acc0
vrossi96 <vrossi396@yahoo.it>
2022-01-10 13:31:17 +0100

    add script in html

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log('JS OK');
+
+// # Mail
+/* 
+1- Crea un array di email;
+2- Chiedi all’utente la sua email.
+3- controlla che sia nella lista di chi può accedere
+4- stampa un messaggio appropriato in console sull’esito del controllo. 
+*/
+
+// # Gioco dei dadi
+/* 
+1- Generare un numero random da 1 a 6 
+1b- sia per il giocatore sia per il computer.
+2- Stabilire il vincitore, in base a chi fa il punteggio più alto.
+3- Stampare in console i tiri di dado e il risultato. */
