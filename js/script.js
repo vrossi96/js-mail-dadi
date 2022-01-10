f5a8bf0326b6e86eb22adfa8879d55d43d794be7
vrossi96 <vrossi396@yahoo.it>
2022-01-10 13:52:41 +0100

    added method

--- js/script.js
+++ js/script.js
@@ -17,8 +17,11 @@ console.log('JS OK');
 
 // ESERCIZIO MAIL
 
+// Creo array con mail
 const mail = ['gianni@gmail.com', 'piero@gmail.com', 'giorgio@gmail.com', 'mario@gmail.com', 'luigi@gmail.com'];
 console.table(mail);
 
-const userEmail = window.prompt('Verifica la tua mail', '@gmail.com');
+// Chiedo la mail all'utente
+const userEmail = window.prompt('Verifica la tua mail', '@gmail.com').trim().toLowerCase();
 console.log(userEmail);
+
